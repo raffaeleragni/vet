645e11bf09b482bd859ee95856bf29fc48b15808
Raffaele Ragni <raffaele.ragni@gmail.com>
2023-09-16 22:24:31 +0200

    remove duplication

--- src/modules/kafka.rs
+++ src/modules/kafka.rs
@@ -15,6 +15,12 @@ pub struct Env {
     pub brokers: String,
 }
 
+struct TopicMetadata {
+    partitions: usize,
+    replicas: usize,
+    min_isr: usize,
+}
+
 impl Env {
     fn admin_client(&self) -> AdminClient<DefaultClientContext> {
         ClientConfig::new()
@@ -30,6 +36,33 @@ impl Env {
             .set("session.timeout.ms", "5000")
             .create()
             .expect("Consumer creation failed")
+    }
+
+    fn topic_metadata(&self, topic: &str) -> TopicMetadata {
+        let consumer = self.consumer();
+        let metadata = consumer
+            .fetch_metadata(Some(topic), Duration::from_secs(1))
+            .expect("Topic not found");
+        let topic_metadata = metadata.topics().iter().next().expect("Topic not found");
+        let partitions = topic_metadata.partitions();
+        let partition_count = partitions.len();
+        let min_isr = partitions
+            .iter()
+            .map(|p| p.isr().iter().min().unwrap())
+            .min()
+            .unwrap()
+            .to_owned() as usize;
+        let replicas = partitions
+            .iter()
+            .map(|p| p.replicas().iter().min().unwrap())
+            .min()
+            .unwrap()
+            .to_owned() as usize;
+        TopicMetadata {
+            partitions: partition_count,
+            replicas,
+            min_isr,
+        }
     }
 }
 
@@ -51,45 +84,20 @@ async fn when_create_topic(env: &mut WorldEnv, topic: String) {
 
 #[then(expr = "kafka topic {string} exists")]
 async fn then_topic_exists(env: &mut WorldEnv, topic: String) {
-    let consumer = env.kafka.consumer();
-    consumer
-        .fetch_metadata(Some(topic.as_str()), Duration::from_secs(1))
-        .expect("Topic not found")
-        .topics()
-        .iter()
-        .next()
-        .expect("Topic not found");
+    env.kafka.topic_metadata(topic.as_str());
 }
 
 #[then(expr = "kafka topic {string} exists with settings:")]
 async fn then_topic_exists_with_settings(env: &mut WorldEnv, topic: String, step: &Step) {
-    let consumer = env.kafka.consumer();
-    let metadata = consumer
-        .fetch_metadata(Some(topic.as_str()), Duration::from_secs(1))
-        .expect("Topic not found");
-    let metadata = metadata.topics().iter().next().expect("Topic not found");
-    let partitions = metadata.partitions();
-    let partition_count = partitions.len();
-    let min_isr = partitions
-        .iter()
-        .map(|p| p.isr().iter().min().unwrap())
-        .min()
-        .unwrap()
-        .to_owned();
-    let replica_count = partitions
-        .iter()
-        .map(|p| p.replicas().iter().min().unwrap())
-        .min()
-        .unwrap()
-        .to_owned();
+    let metadata = env.kafka.topic_metadata(topic.as_str());
     if let Some(table) = step.table.as_ref() {
         for row in table.rows.iter() {
             let key = &row[0];
             let value = &row[1];
             match key.as_str().trim() {
-                "Min ISR" => assert!(value.parse::<i32>().unwrap() <= min_isr),
-                "Replicas" => assert_eq!(replica_count, value.parse::<i32>().unwrap()),
-                "Partitions" => assert_eq!(partition_count, value.parse::<usize>().unwrap()),
+                "Min ISR" => assert!(value.parse::<usize>().unwrap() <= metadata.min_isr),
+                "Replicas" => assert_eq!(metadata.replicas, value.parse::<usize>().unwrap()),
+                "Partitions" => assert_eq!(metadata.partitions, value.parse::<usize>().unwrap()),
                 _ => {}
             }
         }
